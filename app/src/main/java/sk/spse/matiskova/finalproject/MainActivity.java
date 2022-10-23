27cb5adac38ffe740cff7881456eee779f09e835
MichaelaMatiskova <matiskova@spse-po.sk>
2022-10-23 12:54:22 +0200

    add shutdown dark mode

--- app/src/main/java/sk/spse/matiskova/finalproject/MainActivity.java
+++ app/src/main/java/sk/spse/matiskova/finalproject/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import java.io.IOException;
 import java.util.Objects;

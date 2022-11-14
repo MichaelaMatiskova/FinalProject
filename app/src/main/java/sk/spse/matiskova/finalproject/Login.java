1911c0b2afa81f716e54f2ccd12ac4ef65252a48
MichaelaMatiskova <matiskova@spse-po.sk>
2022-11-14 22:37:13 +0100

    add function forgot password

--- app/src/main/java/sk/spse/matiskova/finalproject/Login.java
+++ app/src/main/java/sk/spse/matiskova/finalproject/Login.java
@@ -1,11 +1,14 @@
 package sk.spse.matiskova.finalproject;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -48,7 +53,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         mAuth = FirebaseAuth.getInstance();
 
         forgotPassword = findViewById(R.id.forgotPassword);
-       // forgotPassword.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -61,11 +66,11 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
             case R.id.signIn:
                 userLogin();
-               break;
+                break;
 
-            //case R.id.forgotPassword:
-              //  startActivity(new Intent(this, ForgotPassword.class));
-                //break;
+            case R.id.forgotPassword:
+                forgotPassword();
+                break;
         }
     }
 
@@ -120,6 +125,52 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 else {
                     Toast.makeText(Login.this, "Failed to login! Please check you credentials", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+    }
+
+    private void forgotPassword() {
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset password?");
+                passwordResetDialog.setMessage("\nEnter your email to receive the reset link");
+                passwordResetDialog.setView(resetMail);
+                passwordResetDialog.setIcon(R.drawable.forgot);
+
+                passwordResetDialog.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        final String mail = resetMail.getText().toString().trim();
+
+                        if (TextUtils.isEmpty(mail)) {
+                            Toast.makeText(getApplication(), "Enter valid email address.", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Login.this, "Reset link sent to your email.", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Login.this, "Error ! Reset link not sent." + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+                passwordResetDialog.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Close The Dialog Box
+                    }
+                });
+                passwordResetDialog.create().show();
             }
         });
     }

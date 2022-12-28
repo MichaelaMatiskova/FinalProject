7b8d5052a4e0409fce871cb0cb5d8fd771f2203a
MichaelaMatiskova <matiskova@spse-po.sk>
2022-12-28 22:33:59 +0100

    add learning mode

--- app/src/main/java/sk/spse/matiskova/finalproject/QuestionMain.java
+++ app/src/main/java/sk/spse/matiskova/finalproject/QuestionMain.java
@@ -123,6 +123,7 @@ public class QuestionMain extends AppCompatActivity {
 
         else {
             submitButton.setClickable(false);
+            clicableOFF();
             revealAnswerLearning();
 
             nextButton.setOnClickListener(view -> {
